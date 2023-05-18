e71b1233a34db3162901bbd9be0036164a1d81ce
rtgreen86 <21133365+rtgreen86@users.noreply.github.com>
2023-05-18 22:01:03 +0200

    send HTML

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const http = require('node:http');
 
+const template = require('./views/default');
+
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('Hello World!');
+  res.setHeader('Content-Type', 'text/html');
+  res.write(template('Home', 'Hello World!'));
   res.end();
 });
 
